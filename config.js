7dd2279ccb2e10a5e973bd3eefb2ebad5a12d349
gattspy <squid935@gmail.com>
2026-08-22 22:30:33 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,25 +1,20 @@
 module.exports = {
 
-    // Number of joins required to trigger a raid
+    // Number of members joining during the time window
+    // required to trigger a raid.
     raidJoinThreshold: 8,
 
-    // Time window for raid detection
+    // Time window in seconds.
     raidTimeWindow: 10,
 
-    // Accounts younger than 24 hours are suspicious
-    suspiciousAccountAge: 86400000,
+    // Accounts younger than 24 hours are kicked.
+    suspiciousAccountAge: 24 * 60 * 60 * 1000,
 
-    // Lockdown duration
-    // 5 minutes
-    lockdownDuration: 300000,
+    // How long the server stays locked down.
+    // 5 minutes.
+    lockdownDuration: 5 * 60 * 1000,
 
-    // Quarantine suspicious accounts
-    quarantineSuspiciousAccounts: true,
-
-    // Kick extremely suspicious accounts
-    kickSuspiciousAccounts: false,
-
-    // Accounts younger than 1 hour are extremely suspicious
-    extremeAccountAge: 3600000
+    // Automatically kick accounts younger than 24 hours.
+    kickNewAccounts: true
 
 };
