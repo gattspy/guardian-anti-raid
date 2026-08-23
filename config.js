3f5d436b64882cf012c5f73c4abe47e6f47b4789
gattspy <squid935@gmail.com>
2026-08-22 23:19:12 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,40 @@
 module.exports = {
+    // ========================================
+    // RAID PROTECTION
+    // ========================================
+
+    // Number of joins required to trigger a raid
     raidJoinThreshold: 8,
+
+    // Time window for detecting the raid
+    // 10 seconds
     raidTimeWindow: 10,
 
+    // ========================================
+    // NEW ACCOUNT PROTECTION
+    // ========================================
+
     // Accounts younger than 24 hours are kicked
-    suspiciousAccountAge: 24 * 60 * 60 * 1000,
+    suspiciousAccountAge:
+        24 * 60 * 60 * 1000,
+
+    // Automatically kick accounts younger than 24 hours
     kickNewAccounts: true,
 
-    // Lockdown duration: 5 minutes
-    lockdownDuration: 5 * 60 * 1000,
+    // ========================================
+    // SERVER LOCKDOWN
+    // ========================================
 
-    // Word filter timeout: 24 hours
-    wordTimeoutDuration: 24 * 60 * 60 * 1000
+    // Lockdown automatically ends after 5 minutes
+    lockdownDuration:
+        5 * 60 * 1000,
+
+    // ========================================
+    // BLOCKED WORD PROTECTION
+    // ========================================
+
+    // Users who use a blocked word are timed out
+    // for 24 hours
+    wordTimeoutDuration:
+        24 * 60 * 60 * 1000
 };
